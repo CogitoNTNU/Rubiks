3cbad9e6678fae142f411b4c8418b25dd3f1e945
1rideee <einri-o@online.no>
2024-09-19 13:26:36 +0200

    Clean up code for alg_solver for blind method, add functiondescription

--- Solvers/alg_solver.py
+++ Solvers/alg_solver.py
@@ -1,28 +1,6 @@
 from magiccube.cube import Cube
 from magiccube.optimizer.move_optimizer import MoveOptimizer
-from magiccube.solver.basic.solver_base import SolverException, SolverStage
-from magiccube.solver.basic.solver_stages import (
-    ConditionAction,
-    stage_recenter_down,
-    stage_recenter_front,
-    stage_white_cross,
-    stage_white_corner,
-    stage_2nd_layer,
-    stage_top_cross,
-    stage_order_top_cross,
-    stage_order_top_corners,
-    stage_turn_top_corners,
-)
-
-# import alg_base
-# if __name__ == "__main__":
-#     import alg_base
-# else:
-#     import Solvers.alg_base
-
 import magiccube
-
-Stages = {}
 
 
 # innitialize solver
@@ -33,7 +11,7 @@ class AlgSolver:
         self.scramble = scramble
         self.cornermem = None
         self.centers = None
-        self.unscrambled = unscrambled = magiccube.Cube(
+        self.unscrambled = magiccube.Cube(
             3, "YYYYYYYYYRRRRRRRRRGGGGGGGGGOOOOOOOOOBBBBBBBBBWWWWWWWWW"
         )
 
@@ -42,74 +20,29 @@ class AlgSolver:
         # i.e "BRW" gives 0,0,0 in uscrambled (white bottom yellow top)
         return self.cube.find_piece(piece)
 
-    def getpiece(self, coords=(0, 0, 0)):
-        return self.cube.get_piece(coords)
-
-    def getstate(self):
-        pass
-
-    def findedge(self):
-        pass
-
     def get_home(self, coords):
+        # Finds where a piece from a coordinate belongs
         return self.unscrambled.find_piece(str(self.cube.get_piece(coords)))[0]
-
-    def get_corners(self):
-        """returns coordinates and colors of all corners"""
-        pieces = list(self.cube.get_all_pieces().items())
-        corners = [
-            (key, value)
-            for key, value in pieces
-            if not key[0] % 2 and not key[1] % 2 and not key[2] % 2
-        ]
-        return corners
-
-    def solve(self):
-        pass
 
     def getalg(letter):
         # Based on what letter is given,
         # this finds the algorithm to solve the pieces
         pass
 
-    def getletter(self):
-        # Based on what findpiece returns,
-        # this function gives a memorization letter
-        # where the piece should go
+    def getlettercenter(self, coords):
+        # Gives a memorization letter based on the letterscheme
+        # where the pcenter iece should go
         pass
 
-    def solvecross(self):
-        """Solves the white cross"""
-        i = 0
-        while i < 1000:
-            self.cube.rotate(self.moves[i])
-            if (
-                not self.cube.find_piece("WB")[0] == (1, 0, 0)
-                and not self.cube.find_piece("WB")[0] == (0, 1, 0)
-                and not self.cube.find_piece("WB")[0] == (0, 0, 1)
-            ):
-                self.cube.rotate(self.moves[i])
-                self.cube.rotate(self.moves[i])
-                self.cube.rotate(self.moves[i])
-            else:
-                self.cube.rotate(self.moves[i])
-                self.cube.rotate(self.moves[i])
-                self.cube.rotate(self.moves[i])
-                break
-            i += 1
-        return self.moves[i]
+    def getlettercorner(self, coords):
+        # Gives a memortization based on the letterscheme
+        # where a corner piece should go
+        pass
 
-    def solvef2l(self):
-        """Solves the first two layers of the cube"""
-        faces = self.cube.get_all_faces()
-        corners = self.get_corners()
-
-        print(corners)
-
-        # solve the green and red corner
-        # print(faces)
-        # print(faces[Face.F])
-        return ""
+    def cyclebreak(self):
+        # Solves a cycle by choosing a random piece that hasnt been solved
+        # and moving the buffer to that spot
+        pass
 
 
 c = magiccube.Cube(3, "YYYYYYYYYRRRRRRRRRGGGGGGGGGOOOOOOOOOBBBBBBBBBWWWWWWWWW")
