b22ebc8ea7cedc72b9f39990316e10850bc51187
Even Ytterli Tokle <Even.Ytterli.Tokle@gmail.com>
2024-10-08 21:59:24 +0200

    Feat: Implement functionality for custom cubes

--- frontend/rubiks-cube/src/three-app.js
+++ frontend/rubiks-cube/src/three-app.js
@@ -96,7 +96,6 @@ const makeRotationMatrix4 = rotationMatrix3 => {
 }
 
 const threeApp = () => {
-  
 
   globals.animationSpeed = queryParamInt("animationSpeed", 100, 1000, 750)
   // const NUM_RANDOM_MOVES = queryParamInt("randomMoves", 10, 100, 25)
@@ -264,7 +263,8 @@ const threeApp = () => {
       const cameraZ = globals.cubeSize * 4
       globals.camera.position.set(cameraX, cameraY, cameraZ)
       globals.camera.lookAt(new THREE.Vector3(0, 0, 0))
-      recreateUiPieces()
+      globals.cube = makeCustomCube(cubePreset)
+      createUiPieces()
     }
     // Reset the cube to the solved state
     globals.cube = L.getSolvedCube(globals.cubeSize)
@@ -279,19 +279,48 @@ const threeApp = () => {
     console.log(globals.cube)
 
     // Update global cube state
-    globals.cube = L.makeMoves(testMoves, L.getSolvedCube(globals.cubeSize)) 
+    globals.cube = L.makeMoves(testMoves, L.getSolvedCube(globals.cubeSize))
 
     // Animate the scramble moves
     setTimeout(animateMoves, BEFORE_DELAY, testMoves)
 
   }
 
+  const cubePreset = [
+    { id: 0, x: -1, y: -1, z: -1, faces: { up: "-", down: "B", left: "B", right: "-", front: "-", back: "B" } },
+    { id: 1, x: -1, y: -1, z: 0, faces: { up: "-", down: "B", left: "B", right: "-", front: "-", back: "-" } },
+    { id: 2, x: -1, y: -1, z: 1, faces: { up: "-", down: "B", left: "B", right: "-", front: "B", back: "-" } },
+    { id: 3, x: -1, y: 0, z: -1, faces: { up: "-", down: "-", left: "B", right: "-", front: "-", back: "B" } },
+    { id: 4, x: -1, y: 0, z: 0, faces: { up: "-", down: "-", left: "B", right: "-", front: "-", back: "-" } },
+    { id: 5, x: -1, y: 0, z: 1, faces: { up: "-", down: "-", left: "B", right: "-", front: "B", back: "-" } },
+    { id: 6, x: -1, y: 1, z: -1, faces: { up: "B", down: "-", left: "B", right: "-", front: "-", back: "B" } },
+    { id: 7, x: -1, y: 1, z: 0, faces: { up: "B", down: "-", left: "B", right: "-", front: "-", back: "-" } },
+    { id: 8, x: -1, y: 1, z: 1, faces: { up: "B", down: "-", left: "B", right: "-", front: "B", back: "-" } },
+    { id: 9, x: 0, y: -1, z: -1, faces: { up: "-", down: "B", left: "-", right: "-", front: "-", back: "B" } },
+    { id: 10, x: 0, y: -1, z: 0, faces: { up: "-", down: "B", left: "-", right: "-", front: "-", back: "-" } },
+    { id: 11, x: 0, y: -1, z: 1, faces: { up: "-", down: "B", left: "-", right: "-", front: "B", back: "-" } },
+    { id: 12, x: 0, y: 0, z: -1, faces: { up: "-", down: "-", left: "-", right: "-", front: "-", back: "B" } },
+    { id: 14, x: 0, y: 0, z: 1, faces: { up: "-", down: "-", left: "-", right: "-", front: "B", back: "-" } },
+    { id: 15, x: 0, y: 1, z: -1, faces: { up: "B", down: "-", left: "-", right: "-", front: "-", back: "B" } },
+    { id: 16, x: 0, y: 1, z: 0, faces: { up: "B", down: "-", left: "-", right: "-", front: "-", back: "-" } },
+    { id: 17, x: 0, y: 1, z: 1, faces: { up: "B", down: "-", left: "-", right: "-", front: "B", back: "-" } },
+    { id: 18, x: 1, y: -1, z: -1, faces: { up: "-", down: "B", left: "-", right: "B", front: "-", back: "B" } },
+    { id: 19, x: 1, y: -1, z: 0, faces: { up: "-", down: "B", left: "-", right: "B", front: "-", back: "-" } },
+    { id: 20, x: 1, y: -1, z: 1, faces: { up: "-", down: "B", left: "-", right: "B", front: "B", back: "-" } },
+    { id: 21, x: 1, y: 0, z: -1, faces: { up: "-", down: "-", left: "-", right: "B", front: "-", back: "B" } },
+    { id: 22, x: 1, y: 0, z: 0, faces: { up: "-", down: "-", left: "-", right: "B", front: "-", back: "-" } },
+    { id: 23, x: 1, y: 0, z: 1, faces: { up: "-", down: "-", left: "-", right: "B", front: "B", back: "-" } },
+    { id: 24, x: 1, y: 1, z: -1, faces: { up: "B", down: "-", left: "-", right: "B", front: "-", back: "B" } },
+    { id: 25, x: 1, y: 1, z: 0, faces: { up: "B", down: "-", left: "-", right: "B", front: "-", back: "-" } },
+    { id: 26, x: 1, y: 1, z: 1, faces: { up: "B", down: "-", left: "-", right: "B", front: "B", back: "-" } },
+  ]
+
   const makeCustomCube = (pieces) => {
     // Validate the input pieces
     if (!Array.isArray(pieces)) {
       throw new Error("Invalid input: pieces should be an array");
     }
-  
+
     // Create the cube with the provided pieces
     const cube = pieces.map((piece, index) => ({
       id: index,
@@ -301,10 +330,10 @@ const threeApp = () => {
       faces: piece.faces,
       accTransform3: piece.accTransform3 || R.Identity,
     }));
-  
+
     return cube;
   };
-  
+
   const init = async () => {
 
     const container = document.getElementById("visualisation-container")
@@ -375,10 +404,11 @@ const threeApp = () => {
 
     globals.cube = L.getSolvedCube(globals.cubeSize)
     globals.pieceGeometry = await loadGeometry("/rubiks-cube/cube-bevelled.glb")
+    globals.cube = makeCustomCube(cubePreset)
     createUiPieces()
 
     // initialized scrambled state for api
-    
+
     // innitScramble()
 
     animate()
